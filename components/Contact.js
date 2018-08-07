298e79ce16d6864c4e69f8297fdfc2d1f367e371
Kat Lasota <lasota.kat@gmail.com>
2018-08-07 21:31:54 +0200

    Fix JSX

--- components/Contact.js
+++ components/Contact.js
@@ -8,7 +8,10 @@ var Contact = React.createClass({
         <div className={'contactItem'}>
           <img className={'contactImage'} src={'http://via.placeholder.com/150x150'}/>
           <p className={'contactLabel'}>
-            Imię: {this.props.contact.firstName}
+            Imię: {this.props.contacts.firstName}
+          </p>
+          <p className={'contactLabel'}>
+            Nazwisko: {this.props.contacts.lastName}
           </p>
           <a href={'mailto:' + this.props.item.email}>
             {this.props.item.email}
