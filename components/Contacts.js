7d68eb167710961fa320ab2d37bc4b2294bc6874
Kat Lasota <lasota.kat@gmail.com>
2018-08-07 21:50:27 +0200

    Fix JSX in all files

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,15 +1,40 @@
-var Contacts = React.createClass({
-    propTypes: {
-      items: React.PropTypes.array.isRequired,
-    },
-  
-    render: function() {
-      var contacts = this.props.items.map(function(contact) {
-          return React.createElement(Contact, {item: contact, key: contact.id});
-      });
-  
-      return (
-        React.createElement('ul', {className: 'contactsList'}, contacts)
-      );
-    }
-  });
+var contacts = [
+  {
+    id: 1,
+    firstName: 'Jan',
+    lastName: 'Nowak',
+    email: 'jan.nowak@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'Adam',
+    lastName: 'Kowalski',
+    email: 'adam.kowalski@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Zbigniew',
+    lastName: 'Koziol',
+    email: 'zbigniew.koziol@example.com',
+  }
+];
+
+var contactForm = {
+  firstName: '',
+  lastName: '',
+  email: ''
+};
+
+
+
+class Contacts extends React.Component {
+  propTypes: {
+    items: React.PropTypes.array.isRequired
+  };
+  render() {
+    var contacts = this.props.items.map(function(contact) {
+      return <Contact item={contact} key={contact.id} />;
+    });
+    return <ul className={"contactList"}>{contacts}</ul>;
+  }
+}
