6180b4fba7bf0142bef933e036dcc04a20148860
Kat Lasota <lasota.kat@gmail.com>
2018-08-07 21:03:12 +0200

    Change contactForm.js to JSX

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -5,25 +5,28 @@ var ContactForm = React.createClass({
   
     render: function() {
       return (
-        React.createElement('form', {className: 'contactForm'},
-          React.createElement('input', {
-            type: 'text',
-            placeholder: 'Imię',
-            value: this.props.contact.firstName,
-          }),
-          React.createElement('input', {
-            type: 'text',
-            placeholder: 'Nazwisko',
-            value: this.props.contact.lastName,
-          }),
-          React.createElement('input', {
-            type: 'email',
-            placeholder: 'Email',
-            value: this.props.contact.email,
-          }),
-          React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
-        )
+        <div>
+          <form className={'ContactForm'}>
+            <input
+              type={'text'}
+              placeholder={'Imię'}
+              defaultValue={this.props.contact.firstName}
+            />
+            <input
+              type={'text'}
+              placeholder={'Nazwisko'}
+              defaultValue={this.props.contact.lastName}
+            />
+            <input
+              type={'email'}
+              placeholder={'Email'}
+              defaultValue={this.props.contact.email}
+            />
+            <button type={'submit'}> Dodaj kontakt </button>
+          </form>
+        </div>
       )
+      console.log('formularz')
     },
   })
   
